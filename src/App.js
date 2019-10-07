608a9aae1ce13acd8858428e6c373158e9eb5729
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-07 18:44:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import imagen from './cryptomonedas.png';
+import Formulario from './components/Formulario';
+import Resultado from './components/Resultado';
+import Spinner from './components/Spinner/Spinner';
+import axios from 'axios';
 
 function App() {
+
+  const [moneda, guardarMoneda] = useState('');
+  const [cripto, guardarCripto] = useState('');
+  const [resultado, guardarResultado] = useState({});
+  const [cargando, guardarCargando] = useState(false);
+
+  useEffect (() => {
+    const cotizarCriptomoneda = async () => {
+      if(moneda === "" || cripto ==="") return;
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`;
+      const resultado = await axios.get(url);
+      guardarCargando(true);
+
+      setTimeout(()=>{ 
+        guardarCargando(false)
+        guardarResultado(resultado.data.DISPLAY[cripto][moneda])
+      },3000);
+    }
+
+    cotizarCriptomoneda();
+  }, [moneda, cripto])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <img src={imagen} alt="imagen" className="logotipo"/>
+        </div>
+        <div className="one-half column">
+          <h1>Cotiza criptomonedas al instante</h1>
+          <Formulario guardarMoneda={guardarMoneda} guardarCripto={guardarCripto}/>
+        </div>
+      </div>
+      <div className="row">
+        {cargando ? <Spinner/> : <Resultado resultado={resultado}/>}
+      </div>
     </div>
   );
 }
